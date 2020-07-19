73590c9d7da4d779c036779b0550f19b8333ec98
Justin <ju3tin@hotmail.co.uk>
2020-07-19 06:28:27 +0100

    Update settings.py

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -1,13 +1,6 @@
 import pymongo
 import os
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from flask import Flask, render_template, url_for, flash, redirect, request, abort, session, jsonify, json
-from flask_wtf import FlaskForm
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-from forms import RegistrationForm, LoginForm
-import bcrypt
+
 
 MONGO_URI = os.getenv("MONGO_URI")
 DBS_NAME = "books"
