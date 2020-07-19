47b79b563bee29c6334d9b35c9226adc1aa18b13
Justin <ju3tin@hotmail.co.uk>
2020-07-19 06:17:42 +0100

    Update settings.py

--- flask_qa/settings.py
+++ flask_qa/settings.py
@@ -1,4 +1,17 @@
-import os 
+import pymongo
+import os
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import Flask, render_template, url_for, flash, redirect, request, abort, session, jsonify, json
+from flask_wtf import FlaskForm
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from forms import RegistrationForm, LoginForm
+import bcrypt
+
+MONGO_URI = os.getenv("MONGO_URI")
+DBS_NAME = "books"
+COLLECTION_NAME = "books"
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
